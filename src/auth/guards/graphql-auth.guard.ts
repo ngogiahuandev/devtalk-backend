fa5d75b482d899649b60819b2b8d90e9d17cb2c0
Ngô Gia Huấn <ngogiahuan23122003@gmail.com>
2024-11-28 02:34:50 +0700

    refactor(refactor): change graphql guard logic

--- src/auth/guards/graphql-auth.guard.ts
+++ src/auth/guards/graphql-auth.guard.ts
@@ -1,15 +1,20 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
-import { IsAuthenticated } from '../utils/is-authenticated.util';
+import { GraphQLError } from 'graphql';
+import { AuthService } from 'src/auth/auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly isAuthenticated: IsAuthenticated) {}
+  constructor(private authService: AuthService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const ctx = GqlExecutionContext.create(context);
     const authHeader = ctx.getContext().req.headers['authorization'];
-    const user = await this.isAuthenticated.verifyBearerToken(authHeader);
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      throw new GraphQLError('Missing or invalid Authorization header');
+    }
+    const token = authHeader.split(' ')[1];
+    const user = await this.authService.verifyToken(token);
     ctx.getContext().req.user = user;
     return true;
   }
